d6bc440778a0a04ae1de4c1d288f0c14be9c7e41
Simone Bierti <sbierti@MacBook-Pro-di-Simone.local>
2021-05-17 13:56:42 +0200

    primo commit

---
+++ src/main/java/it/sbierti/prove/cicd/CicdApplication.java
@@ -0,0 +1,13 @@
+package it.sbierti.prove.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApplication.class, args);
+	}
+
+}
